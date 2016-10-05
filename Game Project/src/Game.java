0ca62c5a3552f8488de9a5bd7f7ac98b803062b2
SLares141 <steven.lares@uconn.edu>
2016-10-05 15:55:52 -0400

    Deleted stack singleton

--- Game Project/src/Game.java
+++ Game Project/src/Game.java
@@ -4,67 +4,58 @@ import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Toolkit;
-import java.awt.Window;
-import java.awt.image.BufferStrategy;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
 import javax.swing.JFrame;
-/*
- * I am not sure exactly how we are going to structure the code, 
- * but some things in here will be useful to have in some form.
- */
-public class Game extends JFrame {
+import javax.swing.JPanel;
+
+
+
+public class Game extends JPanel {
 	private Dimension _screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     private double _windowWidth = _screenSize.getWidth();
     private double _windowHeight = _screenSize.getHeight();
-    public static JFrame frame;
-    
-    private static MainMenuState menu;
+    private WindowFrame windowFrame;
+    private Graphics g;
+   
+
 	
-    BufferedImage background= null;
-    
 	public Game() {
 		createWindow();
+		g = windowFrame.getGraphics();
 	}
 	//with the create window method within the game constructor,
 	//we will be able to simplify the setup in the main method.
 	private void createWindow() {
-		frame = new JFrame("RPG");
-		frame.setSize(new Dimension(960,540));
-		frame.setLocationRelativeTo(null);
-		
-		/**
-		 * real fullscreen below, replace setsize method if you want to use this.
-		 * Note: X button doesn't exist
-		*/
-		//frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
-		//frame.setUndecorated(true);
+		windowFrame = WindowFrame.getInstance();
 		
 		
-		frame.setVisible(true);
-        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-	
-	
+	/**
+	public void paintComponent(Graphics g) {
+        g.drawImage(_background, 0, 0, (int) _windowWidth, (int) _windowHeight, null); //draws background image, sets coordinates and width and height
+        g.drawImage(_sprite, _spriteX, _spriteY, 100, 150, null);
+       
+    }
+	*/
 	public static void main(String[] args) {
 		
+        //loop while until game stops running
 		
-        Game g = new Game();
+	
+		Game g = new Game();
       
-        
+       
         
 		HashMap<String, State> stateMap = new HashMap<String, State>();
-		menu = new MainMenuState();
-		stateMap.put("menu", menu);
-		menu.render();
+		stateMap.put("menu", new MainMenuState());
+		
+		
 		
 		StateStack stateStack = new StateStack(stateMap);
 		//the main menu is set as the initial state
 		stateStack.push("menu");
-	
+		
+		
 		
 		//At the end of the main method, there should be the game loop.
 		//The update and render methods are called on the stateStack instance, 
